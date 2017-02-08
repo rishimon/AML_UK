b6680c378fdb43278ad7a8eebcb25456d3a0d1be
rishimon <rishimr@gmail.com>
2017-02-08 14:14:58 +0000

    d

--- package.sh
+++ package.sh
@@ -4,10 +4,9 @@ group_id=hsbc
 artifact_id=rishi
 version_num=1.1
 
-tar -pczf $group_id-$artifact_id-$version_num.tar.gz .
-tar -pczf $group_id-$artifact_id-$version_num.tar.gz . --exclude ".git" 
+tar -pczf $group_id-$artifact_id-$version_num.tar.gz . --exclude ".git"
 
-tar -tvf *.tar.gz
+tar -tvf *.tar.gz | awk -F/ '{if (NF<4) print }'
 
 pwd
 ls -lrt
