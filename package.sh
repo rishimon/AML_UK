0860e49df3a0e1c6673ee4fbc57936236e89b72d
rishimon <rishimr@gmail.com>
2017-02-08 13:26:06 +0000

    f

--- package.sh
+++ package.sh
@@ -5,6 +5,7 @@ artifact_id=rishi
 version_num=1.1
 
 tar -pczf $group_id-$artifact_id-$version_num.tar.gz .
+tar -pczf $group_id-$artifact_id-$version_num.tar.gz . --exclude ".git" 
 
 tar -tvf *.tar.gz
 
